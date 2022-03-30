1fa4a5e7989c1cad37ce56baca70c5aa08666bc3
Michael Lynch <n9971165@qut.edu.au>
2022-03-30 18:18:36 +1000

    set up packages for 31/03 class

---
+++ src/journeyPlannerAnswer/JourneyPlannerTest.java
@@ -0,0 +1,5 @@
+package journeyPlannerAnswer;
+
+public class JourneyPlannerTest {
+
+}
---
+++ src/totaliserAnswer/TotaliserTest.java
@@ -0,0 +1,28 @@
+package totaliserAnswer;
+
+import org.junit.jupiter.api.*;
+import totaliserQuestion.Totaliser;
+
+
+public class TotaliserTest {
+    private Totaliser tot;
+
+    @BeforeEach
+    public void init(){
+        tot = new Totaliser();
+    }
+
+    @Test
+    public void testReset(){
+        // code to test reset
+    }
+
+    @Test
+    public void testGetSum(){
+        // code to test getsum
+    }
+    @Test
+    public void testEnterValue(){
+        // code to test entervalue
+    }
+}
